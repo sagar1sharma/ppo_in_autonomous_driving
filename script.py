96ee7fed331732a018d2107aba1c6fe8ecf6ec62
sagar1sharma <sagarsharma.ss329.ss46@gmail.com>
2025-07-23 18:33:10 +0200

    minor change

--- script.py
+++ script.py
@@ -1,8 +1,10 @@
 import gymnasium as gym
 import highway_env
 
-env = gym.make("highway-v0",render_mode="rgb_array", config={
-    "simulation_frequency": 15,
-    "duration": 40})
+env = gym.make("highway-v0")
+env.configure({
+    "render_mode":"rgb_array",
+    "simulation_frequency":15,
+    "duration":40})
 obs = env.reset()
 env.render()
